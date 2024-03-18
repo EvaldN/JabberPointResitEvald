39faa664a7350c6b99bc76f4702fea796d7818c2
EvaldN <91316779+EvaldN@users.noreply.github.com>
2024-03-18 21:12:19 +0100

    Update MenuController.java

--- src/MenuController.java
+++ src/MenuController.java
@@ -1,8 +1,25 @@
-import java.awt.*;
-import java.awt.event.ActionEvent;
+import java.awt.MenuBar;
+import java.awt.Frame;
+import java.awt.Menu;
+import java.awt.MenuItem;
+import java.awt.MenuShortcut;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import javax.swing.*;
+import javax.swing.JOptionPane;
+
+/**
+ * <p>The controller for the menu</p>
+ *
+ * @author Ian F. Darwin, ian@darwinsys.com
+ * @author Gert Florijn
+ * @author Sylvia Stuurman
+ * @version 1.1 2002/12/17 Gert Florijn
+ * @version 1.2 2003/11/19 Sylvia Stuurman
+ * @version 1.3 2004/08/17 Sylvia Stuurman
+ * @version 1.4 2007/07/16 Sylvia Stuurman
+ * @version 1.5 2010/03/03 Sylvia Stuurman
+ * @version 1.6 2014/05/16 Sylvia Stuurman
+ */
 
 public class MenuController extends MenuBar {
 	private Frame parent;
