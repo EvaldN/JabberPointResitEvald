e263ba0ead57eb3838e9ea45d92cb086d86efa88
EvaldN <91316779+EvaldN@users.noreply.github.com>
2024-03-18 21:07:53 +0100

    Introduced two constants instead of direct message insertion into AboutBox

--- src/AboutBox.java
+++ src/AboutBox.java
@@ -15,17 +15,19 @@ import javax.swing.JOptionPane;
  * @version 1.6 2014/05/16 Sylvia Stuurman
  */
 public class AboutBox {
+	private static final String ABOUT_TITLE = "About JabberPoint";
+	private static final String ABOUT_MESSAGE =
+			"JabberPoint is a primitive slide-show program in Java(tm). It\n" +
+			"is freely copyable as long as you keep this notice and\n" +
+			"the splash screen intact.\n" +
+			"Copyright (c) 1995-1997 by Ian F. Darwin, ian@darwinsys.com.\n" +
+			"Adapted by Gert Florijn (version 1.1) and " +
+			"Sylvia Stuurman (version 1.2 and higher) for the Open" +
+			"University of the Netherlands, 2002 -- now.\n" +
+			"Author's version available from http://www.darwinsys.com/";
 	public static void show(Frame parent) {
-		JOptionPane.showMessageDialog(parent,
-				"JabberPoint is a primitive slide-show program in Java(tm). It\n" +
-						"is freely copyable as long as you keep this notice and\n" +
-						"the splash screen intact.\n" +
-						"Copyright (c) 1995-1997 by Ian F. Darwin, ian@darwinsys.com.\n" +
-						"Adapted by Gert Florijn (version 1.1) and " +
-						"Sylvia Stuurman (version 1.2 and higher) for the Open" +
-						"University of the Netherlands, 2002 -- now.\n" +
-						"Author's version available from http://www.darwinsys.com/",
-				"About JabberPoint",
+		JOptionPane.showMessageDialog(parent, ABOUT_MESSAGE,
+				ABOUT_TITLE,
 				JOptionPane.INFORMATION_MESSAGE
 		);
 	}
